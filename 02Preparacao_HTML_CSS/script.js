80814445b1184334de5d96ef9b2ffe07becba7b3
Leonardo Reis <leorsouza0311@gmail.com>
2024-02-01 14:59:52 -0300

    folder reorganization and solving project 1 preparation module html css

---
+++ 02Preparacao_HTML_CSS/script.js
@@ -0,0 +1,5 @@
+let listaProdutos = [tijolo, cimento, brita, areia, argamassa];
+
+let listaAnoDeNascimentoDeAmigos = [1983, 1955, 1986, 2014, 2017, 2020];
+
+console.log(listaProdutos, listaAnoDeNascimentoDeAmigos);
