ffe2204addef269f3db597b7a0931af5aa1ed77e
Leonardo Reis <leorsouza0311@gmail.com>
2024-02-02 12:02:40 -0300

    Solving project 2

---
+++ 02Preparacao_HTML_CSS/Projeto02/script.js
@@ -0,0 +1,6 @@
+listaMusicos = ['Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa'];
+
+console.log(listaMusicos.length);
+console.log(listaMusicos[2]);
+console.log(listaMusicos[9]);
+console.log(listaMusicos[14]);
