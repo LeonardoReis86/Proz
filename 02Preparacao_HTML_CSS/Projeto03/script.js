19d3d1ae6839dce46abb923fba97244d298b4f40
Leonardo Reis <leorsouza0311@gmail.com>
2024-02-03 11:39:33 -0300

    Solving project 3

---
+++ 02Preparacao_HTML_CSS/Projeto03/script.js
@@ -0,0 +1,5 @@
+const listaProdutos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'];
+
+for (produto of listaProdutos) {
+  console.log(`Temos ${produto} à venda!`);
+}
